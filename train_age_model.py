9c022fd3d0fc765240396ecbe7d0046711e03e13
Ammar Haider <ammarnaqvi561@gmail.com>
2025-09-13 16:03:59 +0200

    train the age

---
+++ train_age_model.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras import layers, models
+
+# Data generators
+train_gen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
+train_data = train_gen.flow_from_directory(
+    'data/age',
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'
+)
+val_data = train_gen.flow_from_directory(
+    'data/age',
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation'
+)
+
+# Simple CNN model
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),
+    layers.MaxPooling2D(2,2),
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D(2,2),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dense(train_data.num_classes, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train
+model.fit(train_data, validation_data=val_data, epochs=10)
+
+# Save model
+model.save('age_model.h5')
